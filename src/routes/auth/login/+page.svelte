6bb8af41613a687cdeb2567220315d78895173e4
Henrik <norheimhenrik@gmail.com>
2023-11-29 18:23:06 +0100

    Fixed typo

--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -4,6 +4,6 @@ import Login from "$lib/Login.svelte";
 
 <div class="flex flex-col space-y-4">
     <h1 class="text-4xl font-bold flex-0">Wavecom</h1>
-    <h2 class="font-semibold my-auto text-2xl">Sign Up</h2>
+    <h2 class="font-semibold my-auto text-2xl">Login</h2>
 </div>
 <Login />
