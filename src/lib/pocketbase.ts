ff2beed4fee44abf85ee853f09b4abaea8402484
Henrik <norheimhenrik@gmail.com>
2023-11-30 20:25:54 +0100

    updated build to be production ready

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -3,13 +3,13 @@ import PocketBase from 'pocketbase';
 import { writable } from 'svelte/store';
 
 // create connection to backend
-export const pb = new PocketBase('http://192.168.0.112:8090');
+export const pb = new PocketBase('https://api.norheimweb.com/');
 
 // set currentUser to be the currently authenticated user
 export const currentUser = writable(pb.authStore.model);
 
 export function createInstance() {
-    return new PocketBase('http://192.168.0.112:8090');
+    return new PocketBase('https://api.norheimweb.com/');
 }
 
 // lgos
